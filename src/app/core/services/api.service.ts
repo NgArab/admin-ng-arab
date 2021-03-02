c707e1cd485cca2ab8864415316b71b3ac44e42b
Omar Hasan <omar.hassan3030@gmail.com>
2021-03-02 14:56:17 +0200

    fix(add question): add space below the form

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { ajax, AjaxResponse } from 'rxjs/ajax';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
